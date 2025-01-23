26cc38f4f918db2a53262af3169aa85d92874ce3
kjhccscrepo <j_kist1@mail.harpercollege.edu>
2025-01-23 14:45:03 -0600

    Add files via upload

---
+++ LBS and OZS.cpp
@@ -0,0 +1,52 @@
+
+#include <iostream>
+
+
+int main()
+{
+    using namespace std;
+    double UserOZinput;
+    int howmanypounds = 0;
+    double MathOZinputVAR;
+    double poundsdecimal;
+
+    cout << "Hello!\n"
+         << "Welcome to the Ounce Conversion Program!\n";
+    
+    do {
+        cout << "Please enter the value of ounces you want to convert: ";
+        cin >> UserOZinput;
+
+        if (cin.fail()) {
+            cin.clear(); cin.ignore(512, '\n');
+            cout << "\nThe character you entered is invalid, please try again...\n";
+        } 
+        else if (UserOZinput <= 0) {
+            cout << "\nThat is not a positive number, please try again...\n";
+        }
+        else;
+
+    } while (UserOZinput <= 0);
+
+    cout << "\nYou enetered " << UserOZinput << "...\n"
+         << "Converting...\n";
+
+    poundsdecimal = (UserOZinput / 16);
+    MathOZinputVAR = (UserOZinput);
+
+
+    while (MathOZinputVAR >= 16) {
+        howmanypounds += 1;
+        MathOZinputVAR -= 16;
+        if (MathOZinputVAR <= 16)
+        {
+            break;
+        }
+    }
+    cout << UserOZinput << " oz is the equivalent to " << howmanypounds << " lb(s) and " << MathOZinputVAR << " oz.\n";
+    cout << UserOZinput << " oz is the equivalent to " << poundsdecimal << " lb(s).\n";
+
+    cout << "Thank you for using the OCP!";
+
+    return 0;
+}
